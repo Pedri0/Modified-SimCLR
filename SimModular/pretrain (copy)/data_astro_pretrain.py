b9818d14db91b8b58910a97f5da45ede911b7a80
Pedro Arturo Flores-Silva <58676472+Pedri0@users.noreply.github.com>
2021-01-29 16:12:40 -0600

    add .py's gibran

---
+++ SimModular/pretrain (copy)/data_astro_pretrain.py
@@ -0,0 +1,68 @@
+import functools
+from absl import flags
+from absl import logging
+from absl.app import FLAGS
+
+import data_util
+import galaxies_data_pretrain as galax
+import tensorflow.compat.v2 as tf
+
+FLAGS = flags.FLAGS
+
+def bulid_input_fn(global_batch_size):
+    """Build input function.
+    Args:
+        global_batch_size: Global batch size.
+        is_training: Whether to build in training mode.
+    Returns:
+        A function that accepts a dict of params and returns a tuple of images and
+        features, to be used as the input_fn in TPUEstimator.
+    """
+    def _input_fn_(input_context):
+        #Inner input function
+        batch_size = input_context.get_per_replica_batch_size(global_batch_size)
+        #deleted loggins
+        preprocess_fn_pretrain = get_preprocess_fn(True, is_pretrain=True)
+        num_classes = 5
+
+        def map_fn(image, label):
+            #Produces multiple transformations of the same batch for pretraining
+            xs = []
+            for _ in range(2):
+                xs.append(preprocess_fn_pretrain(image))
+            image = tf.concat(xs, -1)
+            label = tf.one_hot(label, num_classes)
+            return image, label
+
+        logging.info('Using Astro pretrain data')
+        dataset = galax.get_data_train()
+
+        if input_context.num_input_pipelines > 1:
+            dataset = dataset.shard(input_context.num_input_pipelines, input_context.input_pipeline_id)
+
+        buffer_multiplier = 10
+        dataset = dataset.shuffle(batch_size * buffer_multiplier)
+        dataset = dataset.repeat(-1)
+        dataset = dataset.map(map_fn, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.batch(batch_size, drop_remainder=True)
+        prefetch_buffer_size = 2
+        dataset = dataset.prefetch(prefetch_buffer_size)
+        return dataset
+
+    return _input_fn_
+
+def build_distributed_dataset(batch_size, strategy):
+    input_fn = bulid_input_fn(batch_size)
+    return strategy.distribute_datasets_from_function(input_fn)
+
+def get_preprocess_fn(is_training, is_pretrain):
+    #Get function that accepts an image and returns a preprocessed image
+    test_crop=True
+    return functools.partial(
+        data_util.preprocess_image,
+        height = FLAGS.image_size,
+        width= FLAGS.image_size,
+        is_training=is_training,
+        color_distort=is_pretrain,
+        test_crop=test_crop
+    )
---
+++ SimModular/pretrain (copy)/model_pretrain.py
@@ -0,0 +1,147 @@
+import math
+import tensorflow.compat.v2 as tf
+from absl import flags
+
+import data_util
+import lars_optimizer
+import resnet_pretrain as resnet
+
+FLAGS = flags.FLAGS
+
+########### UTILFUNCTIONS #########################################
+def build_optimizer(learning_rate):
+    #using lars optimizer
+    return lars_optimizer.LARSOptimizer(learning_rate, momentum=FLAGS.momentum, weight_decay=FLAGS.weight_decay,
+        exclude_from_weight_decay=['batch_normalization', 'bias', 'head_supervised'])
+
+def add_weight_decay(model):
+    # Weight decay are taking care of by optimizer for these cases.
+    # Except for supervised head, which will be added here.
+    l2_losses = [tf.nn.l2_loss(v) for v in model.trainable_variables if 'head_supervised' in v.name and
+        'bias' not in v.name]
+    if l2_losses:
+        return FLAGS.weight_decay * tf.add_n(l2_losses)
+    else:
+        return 0
+
+
+class WarmUpAndCosineDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
+    # Applies a warmup schedule on a given learning rate decay schedule
+    #using sqrt learning_rate_scaling
+    def __init__(self, base_learning_rate, num_examples, name=None):
+        super(WarmUpAndCosineDecay,self).__init__()
+        self.base_learning_rate = base_learning_rate
+        self.num_examples = num_examples
+        self._name = name
+
+    def __call__(self, step):
+        with tf.name_scope(self._name or 'WarmUpAndCosineDecay'):
+            warmup_steps = int(round(FLAGS.warmup_epochs * self.num_examples // FLAGS.train_batch_size))
+            scaled_lr = self.base_learning_rate * math.sqrt(FLAGS.train_batch_size)
+            learning_rate = (step / float(warmup_steps) * scaled_lr if warmup_steps else scaled_lr)
+            #Cosine decay learning rate schedule
+            total_steps = FLAGS.train_steps or (self.num_examples * FLAGS.train_epochs // FLAGS.train_batch_size + 1)
+            #TODO: cache this object
+            cosine_decay = tf.keras.experimental.CosineDecay(scaled_lr, total_steps - warmup_steps)
+            learning_rate = tf.where(step < warmup_steps, learning_rate, cosine_decay(step - warmup_steps))
+
+            return learning_rate
+
+    def get_config(self):
+        return {'base_learning_rate': self.base_learning_rate, 'num_examples': self.num_examples,}
+###################################################################
+
+class LinearLayer(tf.keras.layers.Layer):
+    def __init__(self, num_classes, use_bias=True, use_bn=False, name='linear_layer', **kwargs):
+        # Note: use_bias is ignored for the dense layer when use_bn =True. However, it is still used for batch norm
+        super(LinearLayer, self).__init__(**kwargs)
+        self.num_classes = num_classes
+        self.use_bn = use_bn
+        self._name = name
+        if callable(self.num_classes):
+            num_classes = -1
+        else:
+            num_classes = self.num_classes
+        self.dense = tf.keras.layers.Dense(num_classes, kernel_initializer=tf.keras.initializers.RandomNormal(stddev=0.01),
+        use_bias=use_bias and not self.use_bn)
+        if self.use_bn:
+            self.bn_relu = resnet.BatchNormRelu(relu=False, center=use_bias)
+
+    def build(self, input_shape):
+        ## TODO: Add a new SquareDense layer.
+        if callable(self.num_classes):
+            self.dense.units = self.num_classes(input_shape)
+        super(LinearLayer,self).build(input_shape)
+
+    def call(self, inputs, training):
+        assert inputs.shape.ndims == 2, inputs.shape
+        inputs = self.dense(inputs)
+        if self.use_bn:
+            inputs = self.bn_relu(inputs, training=training)
+        return inputs
+
+class ProjectionHead(tf.keras.layers.Layer):
+    #using nonlinear projectionHead
+    def __init__(self, **kwargs):
+        self.linear_layers = []
+        for j in range(FLAGS.num_proj_layers):
+            if j != FLAGS.num_proj_layers - 1:
+                #for the middle layers, use bias and relu for the output
+                self.linear_layers.append(LinearLayer(num_classes=lambda input_shape: int(input_shape[-1]),
+                use_bias=True, use_bn=True, name='nl_%d' % j))
+            else:
+                #for the final layer, neither bias nor relu is used
+                self.linear_layers.append(LinearLayer(num_classes=FLAGS.proj_out_dim, use_bias=False, use_bn=True, name='nl_%d' %j))
+        
+        super(ProjectionHead, self).__init__(**kwargs)
+    
+    def call(self, inputs, training):
+        hiddens_list = [tf.identity(inputs, 'proj_head_input')]
+        for j in range(FLAGS.num_proj_layers):
+            hiddens = self.linear_layers[j](hiddens_list[-1], training)
+            if j!= FLAGS.num_proj_layers - 1:
+                #for the middle layers, use bias and relu for the output.
+                hiddens = tf.nn.relu(hiddens)
+            hiddens_list.append(hiddens)
+
+        #The first element is the output of the projection head.
+        #The second element is the input of the finetune head
+        proj_head_output = tf.identity(hiddens_list[-1], 'proj_head_output')
+        return proj_head_output, hiddens_list[FLAGS.ft_proj_selector]
+
+class SupervisedHead(tf.keras.layers.Layer):
+    def __init__(self, num_classes, name='head_supervised', **kwargs):
+        super(SupervisedHead, self).__init__(name=name, **kwargs)
+        self.linear_layer = LinearLayer(num_classes)
+
+    def call(self, inputs, training):
+        inputs = self.linear_layer(inputs, training)
+        inputs = tf.identity(inputs, name='logits_sup')
+        return inputs
+
+class Model(tf.keras.models.Model):
+    #Resnet model with projection head or supervised layer
+
+    def __init__(self, num_classes, **kwargs):
+        super(Model, self).__init__(**kwargs)
+        #resnet
+        self.resnet_model = resnet.resnet(resnet_depth=FLAGS.resnet_depth, cifar_stem=FLAGS.image_size <=32)
+        self._projection_head = ProjectionHead()
+
+    def __call__(self, inputs, training):
+        features = inputs
+        num_transforms = 2
+                
+        #split channels and optionally apply extra batched augmentation
+        features_list = tf.split(features, num_or_size_splits=num_transforms, axis=-1)
+        if FLAGS.use_blur:
+            features_list = data_util.batch_random_blur(features_list, FLAGS.image_size, FLAGS.image_size)
+        features = tf.concat(features_list, 0) #(num_transforms * bsz, h, w, c)
+
+        #base network forward pass
+        hiddens = self.resnet_model(features, training=training)
+
+        #add heads
+        projection_head_outputs, _ = self._projection_head(hiddens, training)
+
+        return projection_head_outputs
